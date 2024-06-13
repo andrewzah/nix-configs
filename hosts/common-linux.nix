865ac74a59136ddbd17a430959246ced34b62f12
Andrew Zah <zah@andrewzah.com>
2024-06-13 14:48:40 +0900

    fix git alias, mv old nix wallpaper

--- hosts/common-linux.nix
+++ hosts/common-linux.nix
@@ -21,7 +21,7 @@
       aliases = "!git config --list | grep 'alias\\.' | sed 's/alias\\.\\([^=]*\\)=\\(.*\\)/\\1\\ \t => \\2/' | sort";
       churn = "!git log --all -M -C --name-only --format='format:' \"$@\" | sort | grep -v '^$' | uniq -c | sort | awk 'BEGIN {print \"count,file\"} {print $1 \",\" $2}'";
       l = "!f() { git log $* | grep '^commit ' | cut -f 2 -d ' '; }; f";
-      please = "please = push --force-with-lease";
+      please = "push --force-with-lease";
       tags = "tag -l";
       overview = "log --all --oneline --no-merges";
       recap = "log --all --pretty=format:'%h %ad %s' --no-merges --author=<zah@andrewzah.com> --date=short";
