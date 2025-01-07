0f824dbb45db12d3023b33193f6d59e9b8e79bae
Andrew Zah <zah@andrewzah.com>
2025-01-07 11:16:47 -0500

    fix xserver vim copy/paste, install xclip

--- home/x11.nix
+++ home/x11.nix
@@ -3,6 +3,7 @@
     discord
     discordchatexporter-cli
     rofi
+    xclip
   ]);
 
   xsession.windowManager.i3 = {
@@ -16,25 +17,25 @@
       startup = [
         {
           command = ''
-          ${pkgs.xorg.xrandr}/bin/xrandr \
-            --output DP-0 \
-              --mode 3440x1440 \
-              --pos 1440x0 \
-              --rotate normal \
-            --output DP-1 --off \
-            --output HDMI-0 --off \
-            --output DP-2 \
-              --mode 2560x1440 \
-              --pos 0x199 \
-              --rotate right \
-            --output DP-3 --off \
-            --output DP-4 \
-              --primary \
-              --mode 2560x1440 \
-              --rate 144 \
-              --pos 1440x1440 \
-              --rotate normal \
-            --output DP-5 --off \
+            ${pkgs.xorg.xrandr}/bin/xrandr \
+              --output DP-0 \
+                --mode 3440x1440 \
+                --pos 1440x0 \
+                --rotate normal \
+              --output DP-1 --off \
+              --output HDMI-0 --off \
+              --output DP-2 \
+                --mode 2560x1440 \
+                --pos 0x199 \
+                --rotate right \
+              --output DP-3 --off \
+              --output DP-4 \
+                --primary \
+                --mode 2560x1440 \
+                --rate 144 \
+                --pos 1440x1440 \
+                --rotate normal \
+              --output DP-5 --off \
             --output USB-C-0 --off
           '';
           always = true;
