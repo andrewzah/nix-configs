18ebd0cc4792f53e4cf10b672ba30fe7bda1e1c9
Andrew Zah <zah@andrewzah.com>
2025-04-12 17:03:47 +0900

    extraHosts

--- hosts/extraHosts.nix
+++ hosts/extraHosts.nix
@@ -1,13 +1,17 @@
 {pkgs, ...}: {
     networking.extraHosts = pkgs.lib.concatStringsSep "\n" [
       "127.0.0.1 airflow.arpa"
-      "127.0.0.1 amundsen.arpa"
       "127.0.0.1 elasticsearch"
+      "127.0.0.1 keycloak.arpa"
       "127.0.0.1 labelstudio.arpa"
+      "127.0.0.1 nginx.arpa"
       "127.0.0.1 pulp.arpa"
       "127.0.0.1 superset.arpa"
-      "127.0.0.1 nginx.arpa"
 
+      "127.0.0.1 rekor.sigstore.arpa"
+      "127.0.0.1 registry.sigstore.arpa"
+
+      "127.0.0.1 amundsen.arpa"
       "127.0.0.1 metadata.amundsen.arpa"
       "127.0.0.1 search.amundsen.arpa"
 
