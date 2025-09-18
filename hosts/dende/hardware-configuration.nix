d1703678efa1b68073f723a53ddd3586063d912c
Andrew Zah <zah@andrewzah.com>
2025-09-18 14:29:10 +0900

    remove tailscale kernel patch

--- hosts/dende/hardware-configuration.nix
+++ hosts/dende/hardware-configuration.nix
@@ -14,19 +14,7 @@
   boot.initrd.kernelModules = [];
   boot.kernelModules = ["kvm-amd"];
   boot.extraModulePackages = [];
-  boot.kernelPatches = [
-    # Fix the /proc/net/tcp seek issue
-    # Impacts tailscale: https://github.com/tailscale/tailscale/issues/16966
-    # can be removed when using kernel 6.17 AFAICT
-    {
-      name = "proc: fix missing pde_set_flags() for net proc files";
-      patch = pkgs.fetchurl {
-        name = "fix-missing-pde_set_flags-for-net-proc-files.patch";
-        url = "https://patchwork.kernel.org/project/linux-fsdevel/patch/20250821105806.1453833-1-wangzijie1@honor.com/raw/";
-        hash = "sha256-DbQ8FiRj65B28zP0xxg6LvW5ocEH8AHOqaRbYZOTDXg=";
-      };
-    }
-  ];
+  #boot.kernelPatches = [];
 
   fileSystems."/" = {
     device = "/dev/disk/by-uuid/654412d7-5fc8-4030-9bb4-87cae653d3b1";
